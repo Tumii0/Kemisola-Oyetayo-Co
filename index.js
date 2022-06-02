8e8b811189796d91b1279d0a1170148d467a275e
Tumininu <smattumi@gmai.com>
2022-06-02 21:47:37 +0100

    nothing changed

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const navToggle = document.querySelector(".nav-toggle")
 const links = document.querySelector(".links")
 
 navToggle.addEventListener("click", function(){
-    links.classList.toggle("show-links")
-  
+    return toggle()
 })
+
+
+
+
+links.addEventListener("click", function(){
+   return toggle()
+})
+
+
+function toggle(){
+    links.classList.toggle("show-links")
+}
